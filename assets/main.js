4ed5bc19ca8933ebf3cdf94a004dcb45e29705ce
Alan Jenkins <alan.jenkins@claranet.uk>
2017-05-29 09:29:10 +0100

    Completed code.

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,25 @@
 $(function() {
+  var codeschool_report_card_url = "https://www.codeschool.com/users/998004.json";
+  var courses_completed = "";
 
-  // your code will go here
+  $.ajax({
+    url: codeschool_report_card_url,
+    dataType: 'jsonp',
+    success: function(response) {
+      courses_completed = response.courses.completed;
+      $.each(response.courses.completed, function(index, value){
+        var link = "<a href='" + value.url + "' target='_blank' class='btn btn-primary'>See Course</a>";
+        var course_title = "<h3>" + value.title + "<h3>";
+        var course_image = "<img src='" + value.badge + "' alt='" + value.title + " course logo.'/>";
+        var badge_html = "<div class='course'>" +
+        course_title +
+        course_image +
+        link +
+        "</div>";
+        $(badge_html).appendTo($('#badges'));
+      })
+    }
+  });
+
 
 });
